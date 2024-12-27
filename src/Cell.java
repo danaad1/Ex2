70f0f2559a706a9940857e8a8272545a62fe9a7c
laptop1 <danaadler5@gmail.com>
2024-12-27 15:50:58 +0200

    start 1

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,38 @@
+
 public class Cell {
 
+    /**
+     * this function checks if a given string is a number
+     *
+     * @param num
+     * @return
+     */
+    public boolean isNumber (String num){
+        boolean ans = true;
+        for (int i = 1 ; i < num.length(); i++){}
+            {if (num.charAt(i) == '-'){
+                ans = false;
+            }
+        }
+
+        return ans;
+    }
+    public boolean isText (String num){
+        boolean ans = true;
+
+        return ans;
+    }
+
+    public boolean isForm (String num) {
+        boolean ans = true;
+
+        return ans;
+    }
+
+    double computeForm (String num) {
+        double ans = 0;
+
+        return ans;
+
+    }
 }
---
+++ src/CellTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CellTest {
+
+    @org.junit.jupiter.api.Test
+    void isNumber() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isText() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isForm() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void computeForm() {
+    }
+}
